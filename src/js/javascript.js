11f3983ce406b294ab2a824c3b4c6346d7e72f2f
Jonathan López <jonathan@gmail.com>
2020-10-13 11:48:54 -0300

    primer commit

---
+++ src/js/javascript.js
@@ -0,0 +1,10 @@
+$('document').ready(function(){
+  
+$('img').click(function(){
+  var alternativa = $(this).attr('asd');
+  $(this).attr("src",alternativa);
+  // console.log('alt-src value is', alternativa);
+});
+
+
+});
